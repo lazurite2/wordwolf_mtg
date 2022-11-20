b9081b4a0700f171c3096060294a5d529204e558
lazurite2 <qqrq@macbook-air.local>
2022-11-21 00:34:31 +0900

    fix indent

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,9 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Home() {
     const start_button: string = "はじめる";
     const tutorial_button: string = "遊び方";
-    const gametitle: string = "ワードウルフ × MTG"
+    const gametitle: string = "ワードウルフ × MTG";
 
     return (
         <>
@@ -11,7 +11,10 @@ export default function Home() {
                 <h1 className="font-bold">{gametitle}</h1>
             </header>
             <div className="flex flex-col justify-center items-center gap-10">
-                <Link to="/gamesetting" className="p-2 w-2/3 lg:w-1/3 bg-teal-500/60 rounded-md text-center">
+                <Link
+                    to="/gamesetting"
+                    className="p-2 w-2/3 lg:w-1/3 bg-teal-500/60 rounded-md text-center"
+                >
                     {start_button}
                 </Link>
             </div>
